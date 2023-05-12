1343bf82e2fa43479e93c7f4d73d453e21418864
Savchenko <savchenko.ra@yandex.ru>
2023-05-12 13:23:37 +0300

    Final commit.

---
+++ src/SortingService.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface SortingService {
+    void printOddNumbers(List<Integer> numbers);
+
+    void printUniqueEvenNumbersInAscendingOrder(List<Integer> numbers);
+
+    void printUniqueStrings(List<String> strings);
+
+    void printDuplicateStringsCount(List<String> strings);
+}
